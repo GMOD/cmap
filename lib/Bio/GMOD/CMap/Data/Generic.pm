a4e434264307155e46d1a8928c3c81c7c831615c
Ken Youens-Clark <kycl4rk@users.sourceforge.net>
2003-03-28 15:30:08 +0000

    Fixed error in SQL for "feature_detail_data_sql" where "cmap_map_type" wasn't being joined.

--- lib/Bio/GMOD/CMap/Data/Generic.pm
+++ lib/Bio/GMOD/CMap/Data/Generic.pm
@@ -1,6 +1,6 @@
 package Bio::GMOD::CMap::Data::Generic;
 
-# $Id: Generic.pm,v 1.27 2003-03-25 23:52:57 kycl4rk Exp $
+# $Id: Generic.pm,v 1.28 2003-03-28 15:30:08 kycl4rk Exp $
 
 =head1 NAME
 
@@ -31,7 +31,7 @@ drop into the derived class and override a method.
 
 use strict;
 use vars qw( $VERSION );
-$VERSION = (qw$Revision: 1.27 $)[-1];
+$VERSION = (qw$Revision: 1.28 $)[-1];
 
 use Data::Dumper; # really just for debugging
 use Bio::GMOD::CMap;
@@ -623,6 +623,7 @@ The SQL for finding basic info on a feature.
         and    f.map_id=map.map_id
         and    map.map_set_id=ms.map_set_id
         and    ms.species_id=s.species_id
+        and    ms.map_type_id=mt.map_type_id
     ];
 }
 
