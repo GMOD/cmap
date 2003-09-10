62e8ec39f161e714c5948031815987035d3a5c6c
Ken Youens-Clark <kycl4rk@users.sourceforge.net>
2003-09-10 19:57:18 +0000

    Updated with all modules used.

--- conf/perlstartup.pl
+++ conf/perlstartup.pl
@@ -1,17 +1,18 @@
 # File: perlstartup.pl
 use Apache();
 use Apache::Constants();
-use Apache::Cookie();
 use Apache::Request();
 use Cache::FileCache();
 use Class::Base();
-use Cache::Cache();
+use Cache::FileCache();
 use Config::General();
 use Data::Dumper();
+use Data::Page();
 use Data::Pageset();
 use DBI();
 #use DBD::mysql();   # choose the driver that's
 #use DBD::Oracle();  # correct for your installation
+use Digest::MD5();
 use GD();
 use IO::Tee();
 use File::MkTemp();
@@ -20,6 +21,7 @@ use Template();
 use Text::RecordParser();
 use Time::Object();
 use Time::ParseDate();
+use URI::Escape();
 
 use Bio::GMOD::CMap();
 use Bio::GMOD::CMap::Admin();
@@ -29,6 +31,7 @@ use Bio::GMOD::CMap::Apache::HelpViewer();
 use Bio::GMOD::CMap::Apache::Index();
 use Bio::GMOD::CMap::Apache::FeatureSearch();
 use Bio::GMOD::CMap::Apache::FeatureViewer();
+use Bio::GMOD::CMap::Apache::FeatureTypeViewer();
 use Bio::GMOD::CMap::Apache::MapViewer();
 use Bio::GMOD::CMap::Apache::MapDetailViewer();
 use Bio::GMOD::CMap::Apache::MapSetViewer();
