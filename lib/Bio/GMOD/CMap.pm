fa3cd0454dca27743bac1b5e92aabf9a7f47bc6a
Ben Faga <mwz444@users.sourceforge.net>
2004-03-18 22:02:21 +0000

    Made changes to allow the use of Bio::GMOD::CMap::Config which will handle the config files. changed method config() to config_data()

--- lib/Bio/GMOD/CMap.pm
+++ lib/Bio/GMOD/CMap.pm
@@ -1,7 +1,7 @@
 package Bio::GMOD::CMap;
 # vim: set ft=perl:
 
-# $Id: CMap.pm,v 1.48 2004-03-18 16:09:40 mwz444 Exp $
+# $Id: CMap.pm,v 1.49 2004-03-18 22:02:21 mwz444 Exp $
 
 =head1 NAME
 
@@ -35,6 +35,7 @@ use Class::Base;
 use Config::General;
 use Bio::GMOD::CMap::Data;
 use Bio::GMOD::CMap::Constants;
+use Bio::GMOD::CMap::Config;
 use DBI;
 use File::Path;
 use Template;
@@ -44,13 +45,13 @@ use base 'Class::Base';
 # ----------------------------------------------------
 sub init {
     my ( $self, $config ) = @_;
+    $self->{'config'}=$config->{'config'}; 
     $self->data_source( $config->{'data_source'} ) or return;
     return $self;
 }
 
 # ----------------------------------------------------
 sub cache_dir {
-
 =pod
 
 =head2 cache_dir
@@ -62,7 +63,12 @@ Returns the cache directory.
     my $self = shift;
 
     unless ( defined $self->{'cache_dir'} ) {
-        my $cache_dir = $self->config('cache_dir') or return
+	unless($self->{'config'}){
+	    die "no configuration information\n";
+	}
+	
+    
+        my $cache_dir = $self->config_data('cache_dir') or return
             $self->error('No cache directory defined in "'.GLOBAL_CONFIG_FILE.'"');
         unless ( -d $cache_dir ) {
             eval { mkpath( $cache_dir, 0, 0700 ) };
@@ -79,174 +85,27 @@ Returns the cache directory.
     return $self->{'cache_dir'};
 }
 
-#-----------------------------------------------------
-sub read_config_dir {
 
+# ----------------------------------------------------
+sub config_data{
 =pod
 
-=head2 read_config_dir
+=head2 data_source
 
-Reads in config files from the conf directory.
-Requires a global conf and at least one db specific conf.
-The conf dir and the global conf file are specified in Constants.pm
+Access get_config 
 
 =cut
 {
     my $self=shift;
-    my $suffix="conf";
-    my %config_data;
-
-    ###Get files from directory (taken from Bio/Graphics/Browser.pm by lstein)
-    die CONFIG_DIR.": not a directory"  unless -d CONFIG_DIR;
-    opendir(D,CONFIG_DIR) or die "Couldn't open ".CONFIG_DIR.": $!";
-    my @conf_files = map { CONFIG_DIR."/$_" } grep {/\.$suffix$/} readdir(D);
-    close D;
-    
-    # try to work around a bug in Apache/mod_perl which appears when
-    # running under linux/glibc 2.2.1
-    unless (@conf_files) {
-	@conf_files = glob(CONFIG_DIR."/*.$suffix");
+    my $option=shift||'';
+    my $specific_db=shift|'';
+    unless($self->{'config'}){
+	$self->{'config'}=Bio::GMOD::CMap::Config->new();
     }
-    
-    ###read config data from each file and store it all in a hash.
-    foreach my $conf_file (@conf_files){
-	
-	my $conf          = Config::General->new( $conf_file ) or return 0;
-	my %config        = $conf->getall or 
-	    $self->error('No configuration options');
-	if ($conf_file=~/${\GLOBAL_CONFIG_FILE}$/){
-	    $self->{'global_config'} = \%config;
-	}
-	else{
-	    $config_data{$config{'database'}->{'name'}}=\%config;
-	}
-    } 
-    ###Need a global and specific conf file
-    return 0 unless (%config_data and $self->{'global_config'});
-    $self->{'config_data'}=\%config_data;
-    return 1;
-    
-}
-}
-# ----------------------------------------------------
-sub set_config {
-
-=pod
-
-=head2 set_config
-
-sets the active config data (config and data_source)
-
-=cut
-{
-    my $self        = shift;
-    my $config_name = shift;
-    
-    unless ($self->{'config_data'}){
-	$self->read_config_dir() or die "Failed to read config files\n";
-    }
-
-    ###If config_name specified, check if it exists
-    if ($config_name){
-	if ($self->{'config_data'}{$config_name}){
-	    $self->{'config'}=$config_name;
-	    $self->{'data_source'}=$config_name;
-	    return 1;
-	}
-	else{
-	    return 0;
-	}
-    }
-
-    unless ($self->{'config'}){
-	###If the default db is in the global_config
-	### and it exists, set that as the config
-	if ($self->{'global_config'}{'default_db'} 
-	    and $self->{'config_data'}{$self->{'global_config'}{'default_db'}}){
-	    $self->{'config'}=$self->{'global_config'}{'default_db'};
-	    $self->{'data_source'}=$self->{'config'};
-	    return 1;
-	}
-    
-	###No preference set.  Just let fate(keys) decide.
-	$self->{'config'}=${keys %{$self->{'config_data'}}}->[0];
-	$self->{'data_source'}=$self->{'config'};
-	return 1;
-    }
-    return 1;
-
-
+    return $self->{'config'}->get_config($option,$specific_db);
 }
 }
 
-# ----------------------------------------------------
-sub get_config_names {
-
-=pod
-
-=head2 set_config
-
-returns an array ref of the keys to self->{'config_data'}
-
-=cut
-{
-    my $self=shift;
-    return [keys %{$self->{'config_data'}}];
-}
-}
-
-# ----------------------------------------------------
-sub config {
-
-=pod
-
-=head2 config
-
-Returns one option from the config file.
-
-=cut
-{
-    my ( $self, $option,$specific_db) = @_;
-
-    ###If config not set, set it
-    unless ( $self->{'config'} ) {
-        $self->set_config() or die "Failed to set config\n";
-    }
-    
-    if ( $option ) {
-	my $value;
-	###If a specific db conf file was asked for, supply answer from it 
-	if ($specific_db){
-	    $value = defined $self->{'config_data'}->{$specific_db}{ $option } 
-            ?  $self->{'config_data'}->{$specific_db}{ $option }
-	    : '';	    
-	}
-	else{
-	    ###Is it in the global config
-	    if (defined $self->{'global_config'}->{ $option }){
-		$value= $self->{'global_config'}->{ $option };
-	    }
-	    else{
-		###Otherwise get it from the other config.
-		$value = defined $self->{'config_data'}->{$self->{'config'}}{ $option } 
-		?  $self->{'config_data'}->{$self->{'config'}}{ $option }
-		: DEFAULT->{ $option }
-		;
-	    }
-	}
-        if ( $value ) {
-            return wantarray && ref $value ? @$value : $value;
-        }
-        else {
-            return wantarray ? () : '';
-        }
-    }
-    else {
-	###Should never be used
-        return ();
-    }
-}
-}
 # ----------------------------------------------------
 sub data_source {
 
@@ -260,13 +119,17 @@ Basically a front for set_config()
 
     my $self = shift;
     my $arg  = shift || '';
-
+    unless($self->{'config'}){
+	die "No configuration information\n";
+	
+    }
     #
     # If passed a new data source, force a reconnect.
     # This may slow things down.
     #
     if ( $arg ) {
-        $self->set_config($arg) or die "couldn't set data_source:$arg\n";  
+        $self->{'config'}->set_config($arg) or die "couldn't set data_source:$arg\n";  
+	$self->{'data_source'}=$self->config_data('database')->{'name'};
 	if ( defined $self->{'db'} ) {
             my $db = $self->db;
             $db->disconnect;
@@ -274,10 +137,9 @@ Basically a front for set_config()
         }
       
     }
-
-    unless ( $self->{'data_source'} ) {
-        $self->set_config();
-    }
+    unless ($self->{'data_source'}){
+	$self->{'data_source'}=$self->config_data('database')->{'name'};
+    }  
 
     return $self->{'data_source'} || '';
 }
@@ -301,9 +163,10 @@ Returns all the data souces defined in the configuration files.
 	$self->data_source() unless($self->{'data_source'});
         
         my $ok = 0;
+	
         if ( my $current = $self->{'data_source'} ) {
-            foreach my $config_name ( @{$self->get_config_names()} ) {
-		my $source=$self->config('database',$config_name);
+            foreach my $config_name ( @{$self->{'config'}->get_config_names()} ) {
+		my $source=$self->config_data('database',$config_name);
                 if ( $current && $source->{'name'} eq $current ) {
                     $source->{'is_current'} = 1;
                     $ok                     = 1;
@@ -342,11 +205,11 @@ Returns a database handle.  This is the only way into the database.
 =cut
 
     my $self    = shift;
-    my $db_name = shift || $self->data_source;
+    my $db_name = shift || $self->data_source();
     return unless $db_name;
 
     unless ( defined $self->{'db'} ) {
-        my $config = $self->config('database') or 
+        my $config = $self->config_data('database') or 
             return $self->error('No database configuration options defined');
 
         #
@@ -420,7 +283,8 @@ Returns a handle to the data module.
 
     unless ( $self->{'data_module'} ) { 
         $self->{'data_module'} = Bio::GMOD::CMap::Data->new(
-            data_source => $self->data_source,
+            data_source => $self->data_source, 
+	    config => $self->{'config'},
         ) or $self->error( Bio::GMOD::CMap::Data->error );
     }
 
@@ -602,7 +466,7 @@ Returns a Template Toolkit object.
 
     unless ( $self->{'template'} ) {
         my $cache_dir    = $self->cache_dir or return;
-        my $template_dir = $self->config('template_dir') or return
+        my $template_dir = $self->config_data('template_dir') or return
             $self->error('No template directory defined in "'.GLOBAL_CONFIG_FILE.'"');
         return $self->error("Template directory '$template_dir' doesn't exist")
             unless -d $template_dir;
