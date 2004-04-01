7363d9ab4061bf8e0a4a1738b67a9e04bdb832bd
Ben Faga <mwz444@users.sourceforge.net>
2004-04-01 07:50:32 +0000

    Edited to use type accession as the identifier.

--- bin/cmap_typetables_to_config.pl
+++ bin/cmap_typetables_to_config.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: cmap_typetables_to_config.pl,v 1.2 2004-03-26 20:42:10 mwz444 Exp $
+# $Id: cmap_typetables_to_config.pl,v 1.3 2004-04-01 07:50:32 mwz444 Exp $
 
 =head1 NAME
 
@@ -59,37 +59,53 @@ my $sth;
 $sth=$dbh->prepare("select * from cmap_feature_type;");
 $sth->execute;
 while (  my $row = $sth->fetchrow_hashref ) {
-    print "<feature_type ".$row->{'feature_type'}.">\n";
-    print "feature_type ".$row->{'feature_type'}."\n" if $row->{'feature_type'};
-    print "default_rank ".$row->{'default_rank'}."\n" if $row->{'default_rank'};
-    print "color ".$row->{'color'}."\n" if $row->{'color'};
-    print "shape ".$row->{'shape'}."\n" if $row->{'shape'};
-    print "drawing_lane ".$row->{'drawing_lane'}."\n" if $row->{'drawing_lane'};
-    print "drawing_priority ".$row->{'drawing_priority'}."\n" if $row->{'drawing_priority'};
-    print "</feature_type>\n\n";
+    print "\n<feature_type ".$row->{'accession_id'}.">";
+    print "\nfeature_type ";
+    print $row->{'feature_type'} if $row->{'feature_type'};
+    print "\ndefault_rank ";
+    print $row->{'default_rank'} if $row->{'default_rank'};
+    print "\ncolor ";
+    print $row->{'color'} if $row->{'color'};
+    print "\nshape ";
+    print $row->{'shape'} if $row->{'shape'};
+    print "\ndrawing_lane ";
+    print $row->{'drawing_lane'} if $row->{'drawing_lane'};
+    print "\ndrawing_priority ";
+    print $row->{'drawing_priority'} if $row->{'drawing_priority'};
+    print "\n</feature_type>\n\n";
 }
 ###Get evidence types
 $sth=$dbh->prepare("select * from cmap_evidence_type;");
 $sth->execute;
 while (  my $row = $sth->fetchrow_hashref ) {
-    print "<evidence_type ".$row->{'evidence_type'}.">\n";
-    print "evidence_type ".$row->{'evidence_type'}."\n" if $row->{'evidence_type'};
-    print "rank ".$row->{'rank'}."\n" if $row->{'rank'};
-    print "color ".$row->{'line_color'}."\n" if $row->{'line_color'};
-    print "</evidence_type>\n\n";
+    print "\n<evidence_type ".$row->{'accession_id'}.">";
+    print "\nevidence_type ";
+    print $row->{'evidence_type'} if $row->{'evidence_type'};
+    print "\nrank ";
+    print $row->{'rank'} if $row->{'rank'};
+    print "\ncolor ";
+    print $row->{'line_color'} if $row->{'line_color'};
+    print "\n</evidence_type>\n\n";
 }
 
 ###Get map types
 $sth=$dbh->prepare("select * from cmap_map_type;");
 $sth->execute;
 while (  my $row = $sth->fetchrow_hashref ) {
-    print "<map_type ".$row->{'map_type'}.">\n";
-    print "map_type ".$row->{'map_type'}."\n" if $row->{'map_type'};
-    print "map_units ".$row->{'map_units'}."\n" if $row->{'map_units'};
-    print "is_relational_map ".$row->{'is_relational_map'}."\n" if $row->{'is_relational_map'};
-    print "width ".$row->{'width'}."\n" if $row->{'width'};
-    print "shape ".$row->{'shape'}."\n" if $row->{'shape'};
-    print "color ".$row->{'line_color'}."\n" if $row->{'line_color'};
-    print "display_order ".$row->{'display_order'}."\n" if $row->{'display_order'};
-    print "</map_type>\n\n";
+    print "\n<map_type ".$row->{'accession_id'}.">";
+    print "\nmap_type ";
+    print $row->{'map_type'} if $row->{'map_type'};
+    print "\nmap_units ";
+    print $row->{'map_units'} if $row->{'map_units'};
+    print "\nis_relational_map ";
+    print $row->{'is_relational_map'} if $row->{'is_relational_map'};
+    print "\nwidth ";
+    print $row->{'width'} if $row->{'width'};
+    print "\nshape ";
+    print $row->{'shape'} if $row->{'shape'};
+    print "\ncolor ";
+    print $row->{'line_color'} if $row->{'line_color'};
+    print "\ndisplay_order ";
+    print $row->{'display_order'} if $row->{'display_order'};
+    print "\n</map_type>\n\n";
 }
