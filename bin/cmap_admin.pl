91c73353571518c29a1611660587711370e71e6c
Ben Faga <mwz444@users.sourceforge.net>
2005-01-11 23:46:15 +0000

    Made changes that were in the v0.13 patch cmap_admin.pl.050105.v0.13.patch.

--- bin/cmap_admin.pl
+++ bin/cmap_admin.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
 # vim: set ft=perl:
 
-# $Id: cmap_admin.pl,v 1.84 2005-01-05 03:03:39 mwz444 Exp $
+# $Id: cmap_admin.pl,v 1.85 2005-01-11 23:46:15 mwz444 Exp $
 
 use strict;
 use Pod::Usage;
 use Getopt::Long;
 
 use vars qw[ $VERSION ];
-$VERSION = (qw$Revision: 1.84 $)[-1];
+$VERSION = (qw$Revision: 1.85 $)[-1];
 
 #
 # Get command-line options
@@ -767,9 +767,9 @@ sub export_as_text {
     else {
         @map_set_names = ('All');
     }
-
+    my $display_feature_types;
     if ( !@$feature_types ) {
-        $feature_types = [ [ 'All', 'All' ], ];
+        $display_feature_types = [ [ 'All', 'All' ], ];
     }
 
     my $excluded_fields =
@@ -783,7 +783,7 @@ sub export_as_text {
         '  Data source     : ' . $self->data_source,
         "  Map Sets        :\n" . join( "\n", map { "    $_" } @map_set_names ),
         "  Feature Types   :\n"
-          . join( "\n", map { "    $_->[2]" } @$feature_types ),
+          . join( "\n", map { "    $_->[2]" } @$feature_types || @$display_feature_types ),
         "  Exclude Fields  : $excluded_fields",
         "  Directory       : $dir",
         "[Y/n] " );
