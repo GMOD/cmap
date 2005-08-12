0837091c8bcf4da762d15c7a6d3fae0be448176d
Ben Faga <mwz444@users.sourceforge.net>
2005-08-12 15:58:22 +0000

    Script to make changes to a config file to make it v .15 complient.

---
+++ upgrade/upgrade_config_14_to_15.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+
+=pod
+                                                                                                                             
+=head1 NAME
+                                                                                                                             
+upgrade_config_14_to_15.pl
+                                                                                                                             
+=head1 SYNOPSIS
+                                                                                                                             
+  upgrade_config_14_to_15.pl cmap.conf
+                                                                                                                             
+=head1 DESCRIPTION
+                                                                                                                             
+This script will make changes to a config file that are required for version
+15.  It creates a new file that ends in ".15".
+                                                                                                                             
+=head1 AUTHOR
+                                                                                                                             
+Ben Faga E<lt>faga@cshl.eduE<gt>.
+                                                                                                                             
+=cut
+
+
+use strict;
+
+my $file = shift or die "No file given\n";
+
+open FILE_IN, $file or die "Problem with file, $file\n";
+open FILE_OUT, ">".$file.".15";
+
+while(<FILE_IN>){
+    my $line = $_;
+    $line =~ s/_type_accession/_type_acc/;
+    print FILE_OUT $line;
+}
+
+
